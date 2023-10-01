98c6ec43842492b390d780e729bfa87d9895778d
Tharcisse M <mukundayi@gmail.com>
2023-10-01 11:28:57 +0300

    Add files via upload

--- addons/sub_controller/models/res_users.py
+++ addons/sub_controller/models/res_users.py
@@ -47,25 +47,33 @@ class ResUsers(models.Model):
         config_parameter_obj_sudo = self.env["ir.config_parameter"].sudo()
         saas_url = config_parameter_obj_sudo.get_param('saas.manager.url', '')
         subscription = config_parameter_obj_sudo.get_param('saas.subscription.num', '')
-        pod_code= config_parameter_obj_sudo.get_param('saas.pod.code')
+        pod_code = config_parameter_obj_sudo.get_param('saas.pod.code')
         headers = {
             'Content-Type': 'application/json'
         }
         if saas_url and subscription:
-            response = self._do_request('GET', saas_url+'/access_rights', payload_json={'subscription': subscription,'pod_code':pod_code},headers=headers)
+            response = self._do_request('GET', saas_url + '/access_rights',
+                                        payload_json={'subscription': subscription, 'pod_code': pod_code},
+                                        headers=headers)
 
     @api.model
     def update_user_count(self):
         config_parameter_obj_sudo = self.env["ir.config_parameter"].sudo()
         saas_url = config_parameter_obj_sudo.get_param('saas.manager.url', '')
         subscription = config_parameter_obj_sudo.get_param('saas.subscription.num', '')
-        pod_code=config_parameter_obj_sudo.get_param('saas.pod.code', '')
+        pod_code = config_parameter_obj_sudo.get_param('saas.pod.code', '')
         headers = {
             'Content-Type': 'application/json'
         }
         if saas_url and subscription and pod_code:
-            response = self._do_request('GET', saas_url+'/user_count', payload_json={'subscription': subscription,'pod_code':pod_code},headers=headers)
+            response = self._do_request('GET', saas_url + '/user_count',
+                                        payload_json={'subscription': subscription, 'pod_code': pod_code},
+                                        headers=headers)
             response = response.json()
             user = response.get('user_count', 0)
-            if user:
-                config_parameter_obj_sudo.set_param('sub.saas.erp', int(user))
+            expiry_date = response.get('expiry_date', fields.Date.to_string(fields.Date.today()))
+            is_trial = response.get('is_trial', False)
+            _logger.info(f'UPDATED VALUES {response}')
+            config_parameter_obj_sudo.set_param('sub.saas.erp', int(user))
+            config_parameter_obj_sudo.set_param('saas.subscription.enddate', expiry_date)
+            config_parameter_obj_sudo.set_param('saas.subscription.trial', is_trial)
